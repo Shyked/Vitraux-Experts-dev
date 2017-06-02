ff8af0bb118b0a04faa5c493c704164c1c792017
lstiot <ludovic.stiot@utt.fr>
2017-06-02 11:33:03 +0200

    Mise à jour des tests fonctionnels de l'API, ajout du contrôle du type et des données attendues

--- spec/api/getCollectionByCorpus_spec.js
+++ spec/api/getCollectionByCorpus_spec.js
@@ -1,5 +1,15 @@
 var frisby = require('frisby');
-frisby.create('Get Collection by User Benel')
+frisby.create('Get Collection by corpus User Benel')
   .get('http://localhost:3000/api/corpus/Vitraux%20-%20B%C3%A9nel')
   .expectStatus(200)
+  .expectJSONTypes("Vitraux - Bénel", {
+    name: Array,
+	user: Array
+})
+  .expectJSON("Vitraux - Bénel", {
+    name: ["Vitraux - Bénel"]
+})
+  .expectJSON("Vitraux - Bénel", {
+    user: ["aaf"]
+})
 .toss();
