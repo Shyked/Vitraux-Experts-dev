220ad0c2907d4844dceb6debb3c757c032f47bd4
MSI\Clemouuche <clem.sarant@gmail.com>
2017-06-08 11:24:41 +0200

    add doc + vitraux-db.sql

--- README.md
+++ README.md
@@ -16,3 +16,21 @@ npm install -g jasmine-node
 lancer les tests :
 
 jasmine-node spec/api/
+
+//API REST (localhost:3000/api)
+
+**Available services
+
+- (GET) USER_by_username : /users/aaf
+- (GET) CORPUS_by_username : /corpus/Vitraux - Bénel
+- (GET) ITEM_by_id : /corpus/Vitraux - Bénel/00745c3e27d91d00e3d4db33828dd8797e9c2195
+- (GET) VIEWPOINT_by_id : /viewpoint/a76306e4f17ed4f79e7e481eb9a1bd06
+- (POST) ITEM_comment : /corpus/comment with body {id_item: varchar, id_corpus: varchar, user: varchar, text: varchar
+- (GET) ITEM_comments : /corpus/Vitraux - Bénel/00745c3e27d91d00e3d4db33828dd8797e9c2195/comment
+
+**Installation
+
+1. Install mariahdb (https://mariadb.com/downloads)
+2. Launch the script vitraux-db.sql
+3. Run npm install
+4. Try "node app.js" !
---
+++ vitraux-db.sql
@@ -0,0 +1,33 @@
+-- --------------------------------------------------------
+-- Hôte :                        localhost
+-- Version du serveur:           10.1.20-MariaDB - mariadb.org binary distribution
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Export de la structure de la base pour vitraux-db
+CREATE DATABASE IF NOT EXISTS `vitraux-db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `vitraux-db`;
+
+-- Export de la structure de la table vitraux-db. comments
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_item` varchar(200) NOT NULL,
+  `id_corpus` varchar(200) NOT NULL,
+  `user` varchar(100) NOT NULL,
+  `text` varchar(500) NOT NULL DEFAULT '',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Les données exportées n'étaient pas sélectionnées.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
